92e94c3a44ec45970e0ba007a5b2785f2a553926
zainabdnaya <diyanazizo13@gmail.com>
2021-06-04 14:35:12 +0100

    day02

--- day02/ex01/main.cpp
+++ day02/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: zdnaya <zdnaya@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/02 16:55:36 by zdnaya            #+#    #+#             */
-/*   Updated: 2021/06/03 17:55:20 by zdnaya           ###   ########.fr       */
+/*   Updated: 2021/06/04 14:33:57 by zdnaya           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,8 @@
 
 int main(void)
 {
-
-    Fixed   a;
-    Fixed const b(10); 
+    Fixed a;
+    Fixed const b(10);
     Fixed const c(42.42f);
     Fixed const d(b);
     a = Fixed(1234.4321f);
@@ -24,9 +23,16 @@ int main(void)
     std::cout << "b is " << b << std::endl;
     std::cout << "c is " << c << std::endl;
     std::cout << "d is " << d << std::endl;
+    std::cout << " ----------------------------------" << std::endl;
     std::cout << "a is " << a.toInt() << " as integer" << std::endl;
     std::cout << "b is " << b.toInt() << " as integer" << std::endl;
     std::cout << "c is " << c.toInt() << " as integer" << std::endl;
     std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+    std::cout << " -----------------------------------" << std::endl;
+    std::cout << "a is " << a.toFloat() << " is Float" << std::endl;
+    std::cout << "b is " << b.toFloat() << " is Float" << std::endl;
+    std::cout << "c is " << c.toFloat() << " is Float" << std::endl;
+    std::cout << "d is " << d.toFloat() << " is Float" << std::endl;
+
     return 0;
 }
