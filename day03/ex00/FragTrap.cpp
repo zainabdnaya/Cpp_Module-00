ddae3cfb48800b03e56d33e1a436aa4adef47808
zainabdnaya <diyanazizo13@gmail.com>
2021-06-06 16:51:00 +0100

    day03-ex01

--- day03/ex00/FragTrap.cpp
+++ day03/ex00/FragTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: zdnaya <zdnaya@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/04 22:34:51 by zainabdnaya       #+#    #+#             */
-/*   Updated: 2021/06/06 16:05:42 by zdnaya           ###   ########.fr       */
+/*   Updated: 2021/06/06 16:50:21 by zdnaya           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ FragTrap::FragTrap(std::string str)
 	this->ranged_dmg = 20;
 	this->armor_dmg = 5;
 	std::cout << "\t\tEnter a Name : " << std::endl;
-	std::cout << "\t\t\t" << this->name << std::endl;
+	std::cout << YELLOW << "\t\t\t" << this->name << RESET << std::endl;
 }
 
 FragTrap::FragTrap(FragTrap const &equal)
@@ -50,7 +50,7 @@ FragTrap::FragTrap(FragTrap const &equal)
 void FragTrap::meleeAtack(std::string const &target)
 {
 	std::cout << this->name << " attacks " << target << " from a close distance." << std::endl;
-	std::cout << "\t\tEnergy -= 30 " << std::endl;
+	std::cout << RED << "\t\tEnergy -= 30 " << RESET << std::endl;
 	this->energy = this->energy - 30;
 	this->hitp = this->hitp - 30;
 }
@@ -58,7 +58,7 @@ void FragTrap::meleeAtack(std::string const &target)
 void FragTrap::rangedAttack(std::string const &target)
 {
 	std::cout << this->name << " attacks " << target << " form a long  range." << std::endl;
-	std::cout << "\t\tEnergy -= 20 " << std::endl;
+	std::cout << RED << "\t\tEnergy -= 20 " << RESET << std::endl;
 	this->energy = this->energy - 20;
 	this->hitp = this->hitp - 20;
 }
@@ -67,27 +67,26 @@ void FragTrap::beRepair(unsigned int amount)
 {
 	this->armor_dmg = this->armor_dmg - amount;
 	std::cout << "Using armor against one attack, you still have " << this->armor_dmg << std::endl;
-	std::cout << "\t\t\tEnergy += 10" << std::endl;
-	std::cout << "\t\t\thit_point +=10" << std::endl;
+	std::cout << YELLOW << "\t\t\tEnergy += 10" << RESET << std::endl;
+	std::cout << YELLOW << "\t\t\thit_point +=10" << RESET << std::endl;
 	if (this->hitp > 100)
 		this->hitp = this->max_hitp;
 	if (this->energy > 100)
 		this->energy = this->max_energy;
 	this->energy = this->energy + 10;
-
 	this->hitp = this->hitp + 10;
 }
 
 void FragTrap::takeDamage(unsigned int amount)
 {
 	amount = 0;
-	if ( this->hitp < 0)
+	if (this->hitp < 0)
 		this->hitp = 0;
-	std::cout << "hit_point %  = " << this->hitp << std::endl;
-	if ( this->energy < 0)
+	std::cout << YELLOW << "hit_point % = " << this->hitp << std::endl;
+	if (this->energy < 0)
 		this->energy = 0;
 	std::cout << "Energy % = " << this->energy << std::endl;
-	std::cout << "number of armor_dmg left = " << this->armor_dmg << std::endl;
+	std::cout << "number of armor_dmg left = " << this->armor_dmg << RESET << std::endl;
 }
 
 FragTrap &FragTrap::operator=(FragTrap const &cpy)
@@ -107,43 +106,51 @@ FragTrap &FragTrap::operator=(FragTrap const &cpy)
 
 void FragTrap::vaulthunter_dot_exe(std::string const &target)
 {
-	int k;
-
-	for (int i = 0; i < 8; i++)
-		k = rand() % 7;
+	int k = 0;
+	srand(time(NULL));
+	k = rand() % 7;
 	switch (k)
 	{
 	case 1:
-		std::cout << " Take that !" << target << std::endl;
+		std::cout << CYAN << " Take that !" << target << RESET << std::endl;
 		break;
 	case 2:
-		std::cout << "Badass " << target << "?! Aaahhh!" << std::endl;
+		std::cout << CYAN << "Badass " << target << "?! Aaahhh!" << RESET << std::endl;
+		break;
+
 	case 3:
-		std::cout << "Crap, one shot left!" << std::endl;
+		std::cout << CYAN << "Crap, one shot left!" << RESET << std::endl;
+		break;
+
 	case 4:
-		std::cout << "This is why I was built !" << std::endl;
+		std::cout << CYAN << "This is why I was built !" << RESET << std::endl;
+		break;
+
 	case 5:
-		std::cout << "I am a tornado of death and bullets!" << std::endl;
+		std::cout << CYAN << "I am a tornado of death and bullets!" << RESET << std::endl;
+		break;
+
 	case 6:
-		std::cout << "Grenaaaade!" << std::endl;
+		std::cout << CYAN << "Grenaaaade!" << RESET << std::endl;
+		break;
+
 	case 7:
-		std::cout << "Hot potato!" << std::endl;
+		std::cout << CYAN << "Hot potato!" << RESET << std::endl;
 	default:
 		break;
 	}
-	
 }
 
 FragTrap::~FragTrap()
 {
 	if (this->energy == 0)
 	{
-		std::cout << "Game Over!" << std::endl;
+		std::cout << RED << "Game Over!" << RESET << std::endl;
 	}
 	else if (this->energy > 0)
 	{
 		this->level = this->level + 1;
-		std::cout << this->name << " You Win! "
-				  << "You upgrade the Level " << this->level << std::endl;
+		std::cout << GREEN << this->name << " You Win! "
+				  << "You upgrade the Level " << this->level << RESET << std::endl;
 	}
 }
