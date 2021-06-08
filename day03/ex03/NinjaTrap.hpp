215b8d0906a2dd167dde068dc7e3c9aa4fd6e66a
zainabdnaya <diyanazizo13@gmail.com>
2021-06-08 19:10:21 +0100

    ex04

--- day03/ex03/NinjaTrap.hpp
+++ day03/ex03/NinjaTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: zdnaya <zdnaya@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/08 11:20:40 by zdnaya            #+#    #+#             */
-/*   Updated: 2021/06/08 12:18:52 by zdnaya           ###   ########.fr       */
+/*   Updated: 2021/06/08 18:53:12 by zdnaya           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,23 +20,31 @@
 #define GREEN "\033[32m"
 #define YELLOW "\033[33m"
 #define CYAN "\033[36m"
+#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
+#include "FragTrap.hpp"
 
-class NinjaTrap
+class NinjaTrap : public ClapTrap
 {
 private:
-    std::string     name;
-    int             Hit_points;
-    int             Max_hit_points;
-    int             Energy_points;
-    int             Max_energy_points;
-    int             Level;
-    int             melee_damage;
-    int             ranged_damage;
-    int             armor_damage;
+    std::string name;
+    int Hit_points;
+    int Max_hit_points;
+    int Energy_points;
+    int Max_energy_points;
+    int Level;
+    int melee_damage;
+    int ranged_damage;
+    int armor_damage;
+
 public:
     NinjaTrap();
     ~NinjaTrap();
     NinjaTrap(std::string str);
+    void ninjaShoebox(ClapTrap clap);
+    void ninjaShoebox(FragTrap frag);
+    void ninjaShoebox(ScavTrap scav);
+    void ninjaShoebox(NinjaTrap ninja);
 };
 
 #endif
--- day03/ex03/main.cpp
+++ day03/ex03/main.cpp
@@ -6,27 +6,31 @@
 /*   By: zdnaya <zdnaya@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/05 11:10:29 by zdnaya            #+#    #+#             */
-/*   Updated: 2021/06/08 10:17:43 by zdnaya           ###   ########.fr       */
+/*   Updated: 2021/06/08 19:08:51 by zdnaya           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 #include "ScavTrap.hpp"
 #include "FragTrap.hpp"
+#include "NinjaTrap.hpp"
 
 int main()
 {
-    ScavTrap opponent1("BOB");
-    opponent1.rangedAttack("jack");
-    opponent1.meleeAttack("jack");
-    opponent1.challengeNewcomer("jack");
-    opponent1.takeDamage(0);
-    opponent1.beRepaired(1);
+    ScavTrap opponent1;
     std::cout << "-------------------------------------------------" << std::endl;
-    FragTrap opponent2("Marly");
-    opponent2.rangedAttack("jack");
-    opponent2.meleeAttack("jack");
-    opponent2.vaulthunter_dot_exe("jack");
-    opponent2.rangedAttack("jack");
-    opponent2.takeDamage(0);
+    FragTrap opponent2;
+    std::cout << "-------------------------------------------------" << std::endl;
+    ClapTrap clap;
+    std::cout << "-------------------------------------------------" << std::endl;
+    NinjaTrap ninja("hamza");
+    std::cout << "-------------------------------------------------" << std::endl;
+    ninja.ninjaShoebox(opponent2);
+    std::cout << "-------------------------------------------------" << std::endl;
+    ninja.ninjaShoebox(opponent2);
+    std::cout << "-------------------------------------------------" << std::endl;
+    ninja.ninjaShoebox(clap);
+    std::cout << "-------------------------------------------------" << std::endl;
+    ninja.ninjaShoebox(ninja);
+    
 }
