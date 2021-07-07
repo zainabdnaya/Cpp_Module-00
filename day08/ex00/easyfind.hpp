22ecac717dd7957e088abefbcaf3e1d7e5e26888
zainabdnaya <diyanazizo13@gmail.com>
2021-07-07 12:33:49 +0100

    ==>day08-ex01

--- day08/ex00/easyfind.hpp
+++ day08/ex00/easyfind.hpp
@@ -6,17 +6,27 @@
 /*   By: zainabdnayagmail.com <zainabdnayagmail.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/06 11:01:13 by zainabdnaya       #+#    #+#             */
-/*   Updated: 2021/07/06 11:04:38 by zainabdnaya      ###   ########.fr       */
+/*   Updated: 2021/07/07 12:19:37 by zainabdnaya      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 #define EASYFIND_HPP
+#include <iostream>
+#include <vector>
 
 template <typename T>
-void    easyfind(T container, int i)
+void easyfind(T container, int i)
 {
-    
+    for (int j = 0 ; j <  container.size() ;j++)
+    {
+        if (i == container.at(j))
+        {
+            std::cout << "The element does exist and it's: " << i << std::endl;
+            return;
+        }
+    }
+    throw std::exception();
 }
 
 #endif
---
+++ day08/ex00/main.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*              ß                                          :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zainabdnayagmail.com <zainabdnayagmail.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/07 12:02:14 by zainabdnaya       #+#    #+#             */
+/*   Updated: 2021/07/07 12:02:15 by zainabdnaya      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "easyfind.hpp"
+
+
+int main()
+{
+    std::vector<int> numbers;
+    
+ 	for (int i = 1; i <= 10; i++)
+		numbers.push_back(i);
+
+    try
+    {
+        easyfind(numbers,11);
+    }
+    catch(std::exception)
+    {
+        std::cout << "Error: The wanted element does not exist!" << std::endl;
+    }
+    
+}
